884135e4bb3955d4e2117c90ad518c5668ef2398
yachika <yachika@aksinteractive.in>
2022-08-16 17:48:39 +0530

    Project Setup & Added dependencies

---
+++ app/src/main/java/com/aks/nasapictures/NASAPicturesActivity.kt
@@ -0,0 +1,11 @@
+package com.aks.nasapictures
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NASAPicturesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nasapictures)
+    }
+}
